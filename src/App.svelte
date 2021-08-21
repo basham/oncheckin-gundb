8d10ded8cab0400c38112a0025060ee3808fc81f
Chris Basham <chris@bash.am>
2021-08-21 12:12:23 -0400

    Set up Svelte

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<style>
+  :root {
+    --color-black-1: #ccc;
+    --px-1: calc(1rem/16);
+    --size-1: 0.25rem;
+    --size-2: 0.5rem;
+    --border: var(--px-1) solid var(--color-black-1);
+  }
+
+  :global(body) {
+    font-family: system, sans-serif;
+    margin: 0;
+  }
+
+  header {
+    border-bottom: var(--border);
+    padding: var(--size-2);
+  }
+</style>
+
+<header>
+  <div>OnCheckIn</div>
+</header>
+
+<main>
+  <h1>OnCheckIn</h1>
+</main>
--- src/index.js
+++ src/index.js
@@ -1,65 +1,16 @@
-const { Gun } = window
-const { html, render } = lighterhtml
-const { ReplaySubject, combineLatest, isObservable } = rxjs
-const { map, tap } = rxjs.operators
-const { whenAdded } = WhenElements
+import App from './App.svelte'
 
-Gun.chain.$ = function rxjs () {
-  const stream = new ReplaySubject(1)
-  this.on((value) => stream.next(value))
-  return stream
-}
-
-const combineProps = (source) => {
-  const streams = Object.keys(source)
-    .filter((key) => isObservable(source[key]))
-    .map((key) =>
-      source[key].pipe(
-        map((value) => ({ [key]: value }))
-      )
-    )
-  const data = Object.keys(source)
-    .filter((key) => !isObservable(source[key]))
-    .map((key) => ({ [key]: source[key] }))
-    .reduce((prev, curr) => ({ ...prev, ...curr }), {})
-  return combineLatest(streams).pipe(
-    map((props) =>
-      props
-        .reduce((prev, curr) => ({ ...prev, ...curr }), data)
-    )
-  )
-}
-
-function pluralize (value, str) {
-  return `${str}${value === 1 ? '' : 's'}`
-}
-
-const renderComponent = (element, renderer) => (source$) => source$.pipe(
-  tap((props) => render(element, () => renderer(props)))
-)
-
-const db = Gun()
-
-whenAdded('#app', (el) => {
-  const org$ = db.get('org').$()
-  const memberCount$ = org$.pipe(
-    map(({ members }) => Object.keys(members).length)
-  )
-  const sub = combineProps({
-    org: org$,
-    memberCount: memberCount$,
-    foo: 'bar'
-  }).pipe(
-    renderComponent(el, render)
-  ).subscribe()
-  return () => sub.unsubscribe()
-  function render (props) {
-    const { memberCount, org } = props
-    console.log('&&', props)
-    const { name } = org
-    return html`
-      <h1>${name}</h1>
-      <p>${memberCount} ${pluralize(memberCount, 'member')}</p>
-    `
-  }
+const app = new App({
+  target: document.body
 })
+
+export default app
+
+/*
+if (import.meta.hot) {
+  import.meta.hot.accept()
+  import.meta.hot.dispose(() => {
+    app.$destroy()
+  })
+}
+*/
