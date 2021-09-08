0ce36cf904d89aed534e33cf1334e003b20bd1df
Chris Basham <chris@bash.am>
2021-09-08 12:08:15 -0400

    Switch from localStorage to IndexedDB

--- src/gun.js
+++ src/gun.js
@@ -1,10 +1,19 @@
 import Gun from 'gun/gun'
+import 'gun/sea.js'
+import 'gun/lib/radisk.js'
+import 'gun/lib/radix.js'
+import 'gun/lib/store.js'
+import 'gun/lib/rindexed.js'
+
 import 'gun/lib/then.js'
 
-export const gun = Gun([
-  'http://127.0.0.1:8765/gun',
-  'http://192.168.7.99:8765/gun'
-])
+export const gun = Gun({
+  localStorage: false,
+  peers: [
+    'http://127.0.0.1:8765/gun',
+    'http://192.168.7.99:8765/gun'
+  ]
+})
 export default Gun
 
 export async function map (g) {
