b6a3c6a6c46d9398e211b5cbd09e2ba86ad35c54
Chris Basham <chris@bash.am>
2021-09-13 12:01:20 -0400

    Work on SDK

--- src/dat.js
+++ src/dat.js
@@ -1,4 +1,29 @@
-import co from 'co-hyperdrive'
-import sdk from 'hyper-sdk'
+import './hyper-sdk-bundle.js'
+const SDK = window.hyperSDK
 
-console.log(co, sdk)
+export async function init () {
+  const sdk = await SDK({
+    // persist: false,
+    // storage: null
+  })
+  const { Hypercore, Hyperdrive } = sdk
+
+  const core = Hypercore('test', {
+    valueEncoding: 'json'// ,
+    // persist: false,
+    // storage: null
+  })
+
+  await core.append(JSON.stringify({
+    name: 'Christopher' + Math.random()
+  }))
+
+  const c = await core.get(0)
+
+  // const drive = Hyperdrive('another test')
+  // await drive.ready()
+
+  console.log(JSON.parse(c))
+}
+
+init()
