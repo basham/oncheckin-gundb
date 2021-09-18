e22c42a27b303fe804ae1c2423134581439aae19
Chris Basham <chris@bash.am>
2021-09-18 00:05:39 -0400

    Refactor lookup component

---
+++ src/components/lookup.svelte
@@ -0,0 +1,151 @@
+<script>
+  import cuid from 'cuid'
+
+  export let filter = () => true
+  export let label = ''
+  export let onSelected = () => {}
+  export let options = []
+  export let render = () => {}
+
+  const id = cuid()
+  const inputId = `${id}-input`
+  const labelId = `${id}-label`
+  const listboxId = `${id}-listbox`
+  const optionId = `${id}-option`
+  const maxResults = 10
+
+  let query = ''
+  let results = []
+  let selectedIndex = 0
+
+  $: resultsCount = results.length
+
+  function handleInput (event) {
+    const { value } = event.target
+    results = options
+      .filter((option) => filter(value.trim(), option))
+      .slice(0, maxResults)
+    query = value
+    selectedIndex = 0
+  }
+
+  function handleKeyDown (event) {
+    const { key } = event
+    const i = selectedIndex
+    const lastIndex = resultsCount - 1
+    switch (key) {
+      case 'ArrowDown':
+        event.preventDefault()
+        selectedIndex = i === lastIndex ? 0 : i + 1
+        return
+      case 'ArrowUp':
+        event.preventDefault()
+        selectedIndex = i === 0 ? lastIndex : i - 1
+        return
+      case 'Escape':
+        query = ''
+        return
+    }
+  }
+
+  function handleOptionClick () {
+    select()
+  }
+
+  function handleOptionMouseOver (event) {
+    selectedIndex = parseInt(event.target.dataset.index)
+  }
+
+  function handleSubmit (event) {
+    event.preventDefault()
+    if (results.length > 0) {
+      select()
+    }
+  }
+
+  function select () {
+    onSelected(results[selectedIndex])
+    query = ''
+  }
+</script>
+
+<style>
+  .listbox {
+    background-color: var(--color-white);
+    border: var(--border);
+    border-radius: var(--size-1);
+    line-height: var(--lh-1);
+    list-style-type: none;
+    margin: var(--size-1) 0 0;
+    min-width: 10rem;
+    padding: var(--size-1);
+    position: absolute;
+    width: max-content;
+  }
+
+  .option {
+    cursor: pointer;
+    padding: var(--size-1) var(--size-2);
+  }
+
+  .option[aria-selected=true] {
+    background-color: var(--color-black-9);
+    border-radius: var(--size-1);
+    color: var(--color-white);
+  }
+
+  .noResults {
+    padding: var(--size-1) var(--size-2);
+  }
+</style>
+
+<form
+  autocomplete="off"
+  class="u-m-top-4"
+  on:submit={handleSubmit}>
+  <label
+    for={inputId}
+    id={labelId}>
+    {label}
+  </label>
+  <div
+    aria-expanded={false}
+    aria-haspopup="listbox"
+    aria-owns={listboxId}
+    role="combobox">
+    <input
+      aria-activedescendant={`${optionId}-${selectedIndex}`}
+      aria-autocomplete="list"
+      aria-controls={listboxId}
+      id={inputId}
+      on:input={handleInput}
+      on:keydown={handleKeyDown}
+      type="text"
+      value={query}>
+  </div>
+  {#if resultsCount && query.length}
+    <ul
+      aria-labelledby={labelId}
+      class="listbox"
+      id={listboxId}
+      role="listbox">
+      {#each results as option, i}
+        <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+        <li
+          aria-selected={i === selectedIndex}
+          class="option"
+          data-index={i}
+          id={`${optionId}-${i}`}
+          on:click={handleOptionClick}
+          on:mouseover={handleOptionMouseOver}
+          role="option">
+          {render(option)}
+        </li>
+      {/each}
+    </ul>
+  {:else if !resultsCount && query.length}
+    <div class="listbox">
+      <div class="noResults" role="alert">No results</div>
+    </div>
+  {/if}
+</form>
--- src/components/page-event-attendance.svelte
+++ src/components/page-event-attendance.svelte
@@ -2,22 +2,18 @@
   import { attendanceStore, eventStore, orgStore, participantStore } from '../stores.js'
   import Breadcrumbs from './breadcrumbs.svelte'
   import BreadcrumbsItem from './breadcrumbs-item.svelte'
+  import Lookup from './lookup.svelte'
   import Page from './page.svelte'
 
   const params = (new URL(document.location)).searchParams
   const eventId = params.get('id')
-
   const title = 'Edit attendance'
-  const maxResults = 10
+
   let loading = true
   let notFound = false
   let eventName = ''
   let orgName = ''
   let participants = []
-  let query = ''
-  let options = []
-  let selectedIndex = 0
-  $: optionsCount = options.length
 
   load()
 
@@ -33,87 +29,15 @@
     loading = false
   }
 
-  function handleInput (event) {
-    const { value } = event.target
-    options = participants
-      .filter(({ fullName }) =>
-        fullName.toLowerCase().indexOf(value.trim().toLowerCase()) !== -1
-      )
-      .slice(0, maxResults)
-    query = value
-    selectedIndex = 0
+  function filterResult (query, participant) {
+    return participant.fullName.toLowerCase().indexOf(query.toLowerCase()) !== -1
   }
 
-  function handleKeyDown (event) {
-    const { key } = event
-    const i = selectedIndex
-    const lastIndex = optionsCount - 1
-    switch (key) {
-      case 'ArrowDown':
-        event.preventDefault()
-        selectedIndex = i === lastIndex ? 0 : i + 1
-        return
-      case 'ArrowUp':
-        event.preventDefault()
-        selectedIndex = i === 0 ? lastIndex : i - 1
-        return
-      case 'Escape':
-        query = ''
-        return
-    }
-  }
-
-  async function handleOptionClick () {
-    await addParticipant()
-  }
-
-  function handleOptionMouseOver (event) {
-    selectedIndex = parseInt(event.target.dataset.index)
-  }
-
-  async function handleSubmit (event) {
-    event.preventDefault()
-    if (options.length > 0) {
-      await addParticipant()
-    }
-  }
-
-  async function addParticipant () {
-    const participantId = options[selectedIndex].id
-    await attendanceStore.addAttendee(eventId, participantId)
+  async function addParticipant (participant) {
+    await attendanceStore.addAttendee(eventId, participant.id)
     window.location = `./?p=event&id=${eventId}`
   }
 </script>
-
-<style>
-  .listbox {
-    background-color: var(--color-white);
-    border: var(--border);
-    border-radius: var(--size-1);
-    line-height: var(--lh-1);
-    list-style-type: none;
-    margin: var(--size-1) 0 0;
-    min-width: 10rem;
-    padding: var(--size-1);
-    position: absolute;
-    width: max-content;
-  }
-
-  .option {
-    cursor: pointer;
-    padding: var(--size-1) var(--size-2);
-  }
-
-  .option[aria-selected=true] {
-    background-color: var(--color-black-9);
-    border-radius: var(--size-1);
-    color: var(--color-white);
-  }
-
-  .noResults {
-    padding: var(--size-1) var(--size-2);
-  }
-</style>
 
 <Page
   loading={loading}
@@ -126,54 +50,10 @@
     <BreadcrumbsItem isCurrent={true}>{title}</BreadcrumbsItem>
   </Breadcrumbs>
   <h1>{title}</h1>
-  <form
-    autocomplete="off"
-    class="u-m-top-4"
-    on:submit={handleSubmit}>
-    <label
-      for="participantInput"
-      id="participantLabel">
-      Add participant
-    </label>
-    <div
-      aria-expanded={false}
-      aria-haspopup="listbox"
-      aria-owns="participantListbox"
-      role="combobox">
-      <input
-        aria-activedescendant={`option-${selectedIndex}`}
-        aria-autocomplete="list"
-        aria-controls="participantListbox"
-        id="participantInput"
-        on:input={handleInput}
-        on:keydown={handleKeyDown}
-        type="text"
-        value={query}>
-    </div>
-    {#if optionsCount && query.length}
-      <ul
-        aria-labelledby="participantLabel"
-        class="listbox"
-        id="participantListbox"
-        role="listbox">
-        {#each options as option, i}
-          <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-          <li
-            aria-selected={i === selectedIndex}
-            class="option"
-            data-index={i}
-            id={`option-${i}`}
-            on:click={handleOptionClick}
-            on:mouseover={handleOptionMouseOver}
-            role="option">
-            {option.fullName}
-          </li>
-        {/each}
-      </ul>
-    {:else if !optionsCount && query.length}
-      <div class="listbox">
-        <div class="noResults" role="alert">No results</div>
-      </div>
-    {/if}
-  </form>
+  <Lookup
+    filter={filterResult}
+    label="Add participant"
+    onSelected={addParticipant}
+    options={participants}
+    render={({ fullName }) => fullName} />
 </Page>
