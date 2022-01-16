98d8ad052b7622b5d85ca0799de415e099976a8d
Chris Basham <chris@bash.am>
2022-01-16 13:20:47 -0500

    Fix import path

--- src/stores.js
+++ src/stores.js
@@ -1,4 +1,4 @@
-export { default as checkInStore } from '../stores/check-in.js'
-export { default as eventStore } from '../stores/event.js'
-export { default as participantStore } from '../stores/participant.js'
-export { default as workspaceStore } from '../stores/workspace.js'
+export { default as checkInStore } from './stores/check-in.js'
+export { default as eventStore } from './stores/event.js'
+export { default as participantStore } from './stores/participant.js'
+export { default as workspaceStore } from './stores/workspace.js'
