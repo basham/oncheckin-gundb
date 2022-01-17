db0bb3857c33aa43da12bd5af0cf61afcf324964
Chris Basham <chris@bash.am>
2022-01-17 15:56:47 -0500

    Clean up

--- src/components/sync-status.svelte
+++ src/components/sync-status.svelte
@@ -1,21 +1,7 @@
 <script>
   import { format, isToday } from 'date-fns'
-  import safeAwait from 'safe-await'
   import { onMount } from 'svelte'
   import { workspaceStore } from '../stores.js'
-
-  /*
-  onMount(async () => {
-    workspace = await workspaceStore.get()
-    const [err, res] = await safeAwait(fetch(workspace.apiPathsUrl))
-    if (err || !res.ok) {
-      return
-    }
-    const paths = await res.json()
-    const pathsCount = Object.keys(paths).length
-    console.log('###', pathsCount)
-  })
-  */
 
   let syncing = false
   let lastLocalUpdate
