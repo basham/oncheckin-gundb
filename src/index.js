59ca3c6fcb3ea5379e35d765a8baf248467ca415
Chris Basham <chris@bash.am>
2022-01-30 00:54:46 -0500

    Fix import path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { getRoute } from './router.js'
 async function init () {
   try {
     const route = getRoute()
-    const Page = (await import(`../routes/${route.component}.svelte.js`)).default
+    const Page = (await import(`./routes/${route.component}.svelte.js`)).default
     return new Page({
       context: new Map(Object.entries(route)),
       target: document.body
