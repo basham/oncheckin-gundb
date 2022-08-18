cc1bf349e2aa998f31da55fc014a32b41274633f
Chris Basham <chris@bash.am>
2022-08-17 21:22:06 -0400

    Use top-level await

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import { getRoute } from './router.js'
 
-async function init () {
-  try {
-    const { context, file } = getRoute()
-    const Page = (await import(`./routes/${file}.svelte`)).default
-    const target = document.body
-    return new Page({ context, target })
-  } catch (e) {
-    console.error(e)
-  }
-}
-
-init()
+const { context, file } = getRoute()
+const Page = (await import(`./routes/${file}.svelte`)).default
+const target = document.body
+new Page({ context, target })
