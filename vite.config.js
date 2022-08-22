f0f2a25a434f17c40b42e5e8c99c5bb709ee2c64
Chris Basham <chris@bash.am>
2022-08-21 23:51:26 -0400

    Build for ESNext, so top-level await will work

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
   plugins: [
     svelte()
   ],
