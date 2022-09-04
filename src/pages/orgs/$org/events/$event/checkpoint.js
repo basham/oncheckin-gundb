4a465b9a949e0be53968abd1d9cc435eed1dded7
Chris Basham <chris@bash.am>
2022-09-03 23:27:10 -0400

    Fix Checkpoint and Circle pages

--- src/pages/orgs/$org/events/$event/checkpoint.js
+++ src/pages/orgs/$org/events/$event/checkpoint.js
@@ -1,8 +1,10 @@
+import { getEventCheckIns } from '@src/api.js'
+
 export async function get ({ data }) {
-  const { event } = data
+  const { org, event } = data
   const h1 = event.name
   const h2 = 'Checkpoint'
-  const runners = []
+  const runners = await getEventCheckIns(org.id, event.id)
   const template = { h1, h2, runners }
   return { template }
 }
--- src/pages/orgs/$org/events/$event/circle.js
+++ src/pages/orgs/$org/events/$event/circle.js
@@ -1,8 +1,10 @@
+import { getEventCheckIns } from '@src/api.js'
+
 export async function get ({ data }) {
-  const { event } = data
+  const { org, event } = data
   const h1 = event.name
   const h2 = 'Circle'
-  const checkIns = []
+  const checkIns = await getEventCheckIns(org.id, event.id)
   const template = { h1, h2, checkIns }
   return { template }
 }
