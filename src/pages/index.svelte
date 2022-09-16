e702c1f0e33736862c5d471c0b092e5d3b557d62
Chris Basham <chris@bash.am>
2022-09-15 23:48:17 -0400

    Add format script

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -7,25 +7,25 @@
 	<ul class="list u-m-top-6">
 		<li>
 			<strong>Attendance:</strong>
-			 Check in hashers to events, with the app or a printable roster.
+			Check in hashers to events, with the app or a printable roster.
 		</li>
 		<li>
 			<strong>Reports:</strong>
-			 Get summaries of individual events or yearly totals.
+			Get summaries of individual events or yearly totals.
 		</li>
 		<li>
 			<strong>Offline first:</strong>
-			 The app works in your desktop and mobile browser. It works offline, so if
-			there is no network connectivity, you have no limits.
+			The app works in your desktop and mobile browser. It works offline, so if there
+			is no network connectivity, you have no limits.
 		</li>
 		<li>
 			<strong>You manage your own data:</strong>
-			 There is no global database. Your data is independent from everyone else's.
+			There is no global database. Your data is independent from everyone else's.
 			Easily set up free servers to host your data in a few minutes.
 		</li>
 		<li>
 			<strong>Free:</strong>
-			 OnCheckIn is free to use, but we recommend donating to us, if you find this
+			OnCheckIn is free to use, but we recommend donating to us, if you find this
 			software helpful. Contact us if you'd like to suggest or sponsor certain features.
 		</li>
 	</ul>
